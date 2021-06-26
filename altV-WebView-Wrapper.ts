a8abca3fbc038a0d2ffbfd86847b32340ed3c029
Davy <saysu@gmx.de>
2021-06-26 12:56:05 +0200

    Added unsubscribing alt-events

--- altV-WebView-Wrapper.ts
+++ altV-WebView-Wrapper.ts
@@ -1,11 +1,11 @@
-const onStorage: [string, Function[]][] = [];
+var onStorage: [string, { (...args: any[]): void}[]][] = [];
 
 /**
  * Adds a subscription to an Alt:V-Event emitted by client.
  * @param EventName EventName you want to subscribe to.
  * @param AFunction The handling function you want to be executed.
  */
-export function on(EventName: string, AFunction: Function) {
+export function on(EventName: string, AFunction: { (...args: any[]): void}) {
   // console.log(`Incoming>> Name ${EventName}, ${AFunction.toString()}`);
   //@ts-ignore
   alt.on(EventName, AFunction); //eslint-disable-line no-undef
@@ -18,6 +18,23 @@ export function on(EventName: string, AFunction: Function) {
   }
 }
 
+
+export function off(EventName:string, AFunction: { (...args: any[]): void}){
+  //@ts-ignore
+  alt.off(EventName, AFunction) //eslint-disable-line no-undef
+
+ /**
+  *  THIS PART IS BLINDLY IMPLEMENTED. NOT TESTED
+  * */ 
+  //   let existingEntry = onStorage.find((i) => i[0] === EventName);
+  //     if(!existingEntry){
+  //     console.log(`No event-handling found for event-name ${EventName}`)
+  //     return;
+  //   }
+  //   existingEntry[1] = existingEntry[1].filter(f=>f!==AFunction);
+  
+}
+
 /**
  * Emits an event which has to be handeled by the client.
  * @param EventName EventName you want to send.
@@ -27,11 +44,16 @@ export function emit(EventName: string, ...Parameters: any[]) {
   // @ts-ignore
   alt.emit(EventName, ...Parameters);
 
-  // console.log(`${EventName}: ${Parameters.join(", ")}`);
+  /**
+   * To mock a server-callback you can just check the event-name and emulate datachanges.
+   * Use fireAltEvent() to trigger the callback-event
+   */
 }
 
+
 /**
- * If you are smart you might made your UI accessible for (non automatic) tests and use this function to mock client-event calls. You may use this function in your index.html.
+ * If you are smart you might made your UI accessible for (non automatic) tests and use this function to mock client-event calls. 
+ * You may use this function in your index.html (or where the Root-Component will be mounted).
  * @param EventName
  * @param args
  */
