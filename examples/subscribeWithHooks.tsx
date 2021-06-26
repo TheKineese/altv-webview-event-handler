425fd22500662ff0de251f7c154ad5891f8b23f3
Davy <saysu@gmx.de>
2021-06-26 12:55:37 +0200

    Added ReactHook example

---
+++ examples/subscribeWithHooks.tsx
@@ -0,0 +1,26 @@
+import * as alt from "alt-client";
+interface ExampleWithHooksProps{
+
+}
+function ExampleWithHooks(props:ExampleWithHooksProps){
+
+    const [_aStateProperty, _setAStateProperty] = React.useState<AClass>();
+    const aStateProperty = React.useRef(_aStateProperty);
+
+    const setAStateProperty =(newVal:AClass)=>{
+        _aStateProperty.current = newVal;
+        _setAStateProperty(newVal);
+    }
+
+    // MOUNTING THE COMPONENT --> SUBSCRIBING TO INCOMING EVENTS
+    // UNMOUNTING THE COMPONENT --> UNSUBSCRIBE TO INCOMING EVENTS
+    React.useEffect(()=>{
+        alt.on("UpdateAProperty", setAStateProperty);
+        return ()=>{
+            alt.off("UPdateAProperty", setAStateProperty);
+        }
+    },[])
+
+
+    return <div>...</div>
+}
