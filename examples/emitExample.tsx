6396a2aca0750852ad33d5dc2bbac74d06c49fb2
Davy <saysu@gmx.de>
2021-06-26 12:36:03 +0200

    moved the examples into folder

--- examples/emitExample.tsx
+++ examples/emitExample.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as AltWrapper from "./altV-WebView-Wrapper";
+import * as AltWrapper from "../altV-WebView-Wrapper";
 
 export interface EmitExampleProps {}
 
